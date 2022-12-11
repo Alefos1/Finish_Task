ddc1a428acf82fbf4bca2d6aa685d6489bd8f6c7
Daniil Kolesnikov <danil-kolesnikov@mail.ru>
2022-12-11 16:24:32 +0300

    Programm task 1

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿
+string[] arr = new string[]{
+    "hello", "2", "world", ":-)" 
+};
+
+for (int i = 0; i < arr.Length; i++)
+Console.Write(arr [i] + " ");
+
+for (int i = 0; i < arr.Length; i++)
+if (arr [i].Length >= 4)
+arr [i] = "";
+
+Console.WriteLine(" ");
+
+for (int i = 0; i < arr.Length; i++)
+Console.Write(arr [i] + " ");
